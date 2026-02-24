5d23d62ac86284a78eee83e2abbdd7b056677526
Blessing Ondoro <ondoroblessings@gmail.com>
2026-02-24 10:40:20 +0300

    fix: use HashRouter to prevent refresh 404

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { store } from './app/store';
 import App from './App';
@@ -12,10 +12,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
           <ToastHost />
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     </HelmetProvider>
   </React.StrictMode>
